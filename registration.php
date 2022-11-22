00a4165e4667b54c15bb6e0c0eb4d27438e55634
Albert Shen <albertshen1206@gmail.com>
2022-11-23 00:32:15 +0800

    clean

--- registration.php
+++ registration.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright © PHP Digital, Inc. All rights reserved.
  */
-
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'AlbertMage_Webapi', __DIR__);
